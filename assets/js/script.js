8ec965ce2396df227488ef7fe40a9f1af13456e4
MakottoSan <diogomakotto@gmail.com>
2022-05-04 19:54:12 -0300

    testando nó body

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 function changeMode() {
-    console.log('Teste evento');
+    
 }
 
 const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+
+console.log(body);
 
 button.addEventListener('click', changeMode);
